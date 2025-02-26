a81190d5c9b07ce2197ef8af8f6071ac20165ae8
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2025-02-26 23:12:17 +0530

    ui added

--- stockManager/invex/urls.py
+++ stockManager/invex/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    path('',views.allProducts),
     path('addProduct',views.addProduct),
     path('allProducts',views.allProducts),
     path('editProduct',views.editProduct),
